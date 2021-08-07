e0fdcd86237fae82a570e438d5eea3c5cf891f57
abhinna1 <manangharabhinna@gmail.com>
2021-08-07 20:41:09 +0545

    test commit

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
-class Master:
-    def __init__(self):
-        self.__name = ""
-    def set_name(self, name):
-        self.__name= name
-    def get_name(self):
-        return self.__name
+class Vehicle:
+    def __init__(self, brand, color, age):
+        self.__brand = brand
+        self.__color = color
+        self.__age = age
 
-m1 = Master()
-m1.set_name("Abhinna")
-print(m1.get_name)
+    def set_brand(brand):
+        self.__brand=brand
+    
+    def get_brand(brand):
+        return self.__brand
+    
+    def set_color(color):
+        self.__color=color
+    def get_color():
+        return self.__color
+    
+    def set_age(age):
+        self.__age = age
+    def get_age():
+        return self.__age
+
+
+class Car(Vehicle):
+    def __init__(self, brand, color, age):
+        pass
+
+
+car1 = Vehicle("Suzuki", "Grey", 5)
+print(car1.get_age)
